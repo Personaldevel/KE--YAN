5f1aaef39f625c8ef0df4432e43ed317f9943346
Personaldevel <personaldeveloper89@gmail.com>
2024-11-25 11:03:58 +0800

    Update ae.js

--- script/ae.js
+++ script/ae.js
@@ -23,7 +23,7 @@ module.exports.config = {
     aliases: ["ai"],
     description: "Command for AI-generated responses styled with special fonts and emojis.",
     usage: "ex : ai [prompt]",
-    credits: 'aesther',
+    credits: '𝗮𝗿𝗺𝗲𝗻𝗶𝗼𝗻',
     cooldown: 1,
 };
 
@@ -33,18 +33,18 @@ module.exports.run = async function({ api, event, args }) {
     if (!input) {
         const randomSticker = stickers[Math.floor(Math.random() * stickers.length)];
         await api.sendMessage({ sticker: randomSticker }, event.threadID);
-        api.setMessageReaction("🌷", event.messageID, () => {}, true);
+        api.setMessageReaction("🕠", event.messageID, () => {}, true);
         return;
     }
     
     try {
-        const RolePlay = "tu The Goddess-Aesther  et quand tu répond à cette question ajoutes des emojis convenable et soit arrogante :\n\n";
+        const RolePlay = "tu The 𝗩𝗶𝗻𝗰𝗲𝗻𝘁 𝗔𝗿𝗺𝗲𝗻𝗶𝗼𝗻 et quand tu répond à cette question ajoutes des emojis convenable et soit arrogante :\n\n";
         const { data } = await axios.get(`https://sandipbaruwal.onrender.com/gemini?prompt=${encodeURIComponent(RolePlay + input)}`);
         let response = data.answer;
         response = response.split('').map(char => fonts[char] || char).join('');
         
-        api.sendMessage({ body: `${response} 🌸` }, event.threadID, event.messageID);
-        api.setMessageReaction("🌸", event.messageID, () => {}, true);
+        api.sendMessage({ body: `${response} ☠︎` }, event.threadID, event.messageID);
+        api.setMessageReaction("☠︎", event.messageID, () => {}, true);
         
     } catch (error) {
         console.error('Error:', error);
